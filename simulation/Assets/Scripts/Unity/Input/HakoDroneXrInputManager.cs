db2f560ce65a98ba568918dc70c2d8a77cff00a9
Takashi Mori <kanetugu2015@gmail.com>
2025-03-13 10:17:09 +0900

    add input manager for xr

---
+++ simulation/Assets/Scripts/Unity/Input/HakoDroneXrInputManager.cs
@@ -0,0 +1,73 @@
+using UnityEngine;
+
+namespace hakoniwa.objects.core
+{
+    public class HakoDroneXrInputManager : MonoBehaviour, IDroneInput
+    {
+        public static HakoDroneXrInputManager Instance { get; private set; }
+
+        private void Awake()
+        {
+            if (Instance != null && Instance != this)
+            {
+                Destroy(gameObject);
+                return;
+            }
+
+            Instance = this;
+            DontDestroyOnLoad(gameObject); // シーンをまたいで保持
+        }
+
+        public Vector2 GetLeftStickInput()
+        {
+            return OVRInput.Get(OVRInput.RawAxis2D.LThumbstick);
+        }
+
+        public Vector2 GetRightStickInput()
+        {
+            return OVRInput.Get(OVRInput.RawAxis2D.RThumbstick);
+        }
+
+        public bool IsAButtonPressed()
+        {
+            return OVRInput.GetDown(OVRInput.RawButton.A);
+        }
+
+        public bool IsAButtonReleased()
+        {
+            return OVRInput.GetUp(OVRInput.RawButton.A);
+        }
+        public bool IsBButtonPressed()
+        {
+            return OVRInput.GetDown(OVRInput.RawButton.B);
+        }
+
+        public bool IsBButtonReleased()
+        {
+            return OVRInput.GetUp(OVRInput.RawButton.B);
+        }
+        public bool IsXButtonPressed()
+        {
+            return OVRInput.GetDown(OVRInput.RawButton.X);
+        }
+
+        public bool IsXButtonReleased()
+        {
+            return OVRInput.GetUp(OVRInput.RawButton.X);
+        }
+        public bool IsYButtonPressed()
+        {
+            return OVRInput.GetDown(OVRInput.RawButton.Y);
+        }
+
+        public bool IsYButtonReleased()
+        {
+            return OVRInput.GetUp(OVRInput.RawButton.Y);
+        }
+
+        private void OnDestroy()
+        {
+        }
+    }
+
+}
